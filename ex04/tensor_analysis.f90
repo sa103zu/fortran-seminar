9cdd8ad15a97d6a2d72467f8b6b6da1b35bfad49
sa103zu <sa103zu_3026@keio.jp>
2026-03-16 12:02:20 +0900

    refactor: 組み込み関数を削除し、do ループを使用して明示的に記述

--- ex04/tensor_analysis.f90
+++ ex04/tensor_analysis.f90
@@ -10,6 +10,7 @@ program tensor_calc
     integer :: ans_scalar
     integer :: ans_vec(3)
     integer :: ans_mat(3,3)
+    integer :: i, j, k, l  ! 指標表示のループ計算用変数
 
     ! ==========================================
     ! 1. 初期値の設定
@@ -33,47 +34,93 @@ program tensor_calc
     write(output_unit, *) ""
 
     ! 1) u・v (内積)
-    ans_scalar = dot_product(u, v)
-    write(output_unit, *) "1) u . v"
-    write(output_unit, '(G0)') ans_scalar
+    ! 指標表示: u_i v_i
+    ans_scalar = 0
+    do i = 1, 3
+        ans_scalar = ans_scalar + u(i) * v(i)
+    end do
+    call print_scalar("1) u . v", "u_i v_i", ans_scalar)
 
     write(output_unit, *) ""
 
     ! 2) u × v (外積)
-    ans_vec = cross_product(u, v)
-    call print_vec("2) u x v", ans_vec)
+    ! 指標表示: e_ijk u_j v_k
+    ans_vec = 0
+    do i = 1, 3
+        do j = 1, 3
+            do k = 1, 3
+                ans_vec(i) = ans_vec(i) + eps(i,j,k) * u(j) * v(k)
+            end do
+        end do
+    end do
+    call print_vec("2) u x v", "e_ijk u_j v_k", ans_vec)
 
     write(output_unit, *) ""
 
     ! 3) u (x) v (テンソル積 / 直積)
-    ans_mat = dyadic_product(u, v)
-    call print_mat("3) u (x) v", ans_mat)
+    ! 指標表示: u_i v_j
+    ans_mat = 0
+    do i = 1, 3
+        do j = 1, 3
+            ans_mat(i,j) = u(i) * v(j)
+        end do
+    end do
+    call print_mat("3) u (x) v", "u_i v_j", ans_mat)
 
     write(output_unit, *) ""
 
     ! 4) Su (テンソルとベクトルの積)
-    ans_vec = matmul(S_mat, u)
-    call print_vec("4) S u", ans_vec)
+    ! 指標表示: S_ij u_j
+    ans_vec = 0
+    do i = 1, 3
+        do j = 1, 3
+            ans_vec(i) = ans_vec(i) + S_mat(i,j) * u(j)
+        end do
+    end do
+    call print_vec("4) S u", "S_ij u_j", ans_vec)
 
     write(output_unit, *) ""
 
     ! 5) S × u (テンソルとベクトルの外積)
-    ans_mat = tensor_cross_vec(S_mat, u)
-    call print_mat("5) S x u", ans_mat)
+    ! 指標表示: S_ik e_jkl u_l
+    ans_mat = 0
+    do i = 1, 3
+        do j = 1, 3
+            do k = 1, 3
+                do l = 1, 3
+                    ans_mat(i,j) = ans_mat(i,j) + S_mat(i,k) * eps(j,k,l) * u(l)
+                end do
+            end do
+        end do
+    end do
+    call print_mat("5) S x u", "S_ik e_jkl u_l", ans_mat)
 
     write(output_unit, *) ""
 
     ! 6) ST (テンソル同士の積)
-    ans_mat = matmul(S_mat, T_mat)
-    call print_mat("6) S T", ans_mat)
+    ! 指標表示: S_ik T_kj
+    ans_mat = 0
+    do i = 1, 3
+        do j = 1, 3
+            do k = 1, 3
+                ans_mat(i,j) = ans_mat(i,j) + S_mat(i,k) * T_mat(k,j)
+            end do
+        end do
+    end do
+    call print_mat("6) S T", "S_ik T_kj", ans_mat)
 
     write(output_unit, *) ""
 
     ! 7) S・T (テンソルの内積)
-    ! Σ(S_ij * T_ij) なので、配列の要素ごとの積の和をとる
-    ans_scalar = sum(S_mat * T_mat)
-    write(output_unit, *) "7) S . T"
-    write(output_unit, '(G0)') ans_scalar
+    ! 指標表示: S_ij T_ij
+    ans_scalar = 0
+    do i = 1, 3
+        do j = 1, 3
+            ans_scalar = ans_scalar + S_mat(i,j) * T_mat(i,j)
+        end do
+    end do
+    call print_scalar("7) S . T", "S_ij T_ij", ans_scalar)
+
 
     ! ==========================================
     ! 3. 課題8: 演習問題 1.2の計算 (1〜3のみ)
@@ -83,99 +130,82 @@ program tensor_calc
     write(output_unit, *) ""
 
     ! 1) u・T v
-    ans_scalar = dot_product(u, matmul(T_mat, v))
-    write(output_unit, *) "1.2-1) u . T v"
-    write(output_unit, '(G0)') ans_scalar
+    ! 指標表示: u_i v_j T_ij
+    ans_scalar = 0
+    do i = 1, 3
+        do j = 1, 3
+            ans_scalar = ans_scalar + u(i) * v(j) * T_mat(i,j)
+        end do
+    end do
+    call print_scalar("1.2-1) u . T v", "u_i v_j T_ij", ans_scalar)
 
     write(output_unit, *) ""
 
     ! 2) S^T T^T u
-    ans_vec = matmul(transpose(S_mat), matmul(transpose(T_mat), u))
-    call print_vec("1.2-2) S^T T^T u", ans_vec)
+    ! 指標表示: S_ji u_k T_kj
+    ans_vec = 0
+    do i = 1, 3
+        do j = 1, 3
+            do k = 1, 3
+                ans_vec(i) = ans_vec(i) + S_mat(j,i) * u(k) * T_mat(k,j)
+            end do
+        end do
+    end do
+    call print_vec("1.2-2) S^T T^T u", "S_ji u_k T_kj", ans_vec)
 
     write(output_unit, *) ""
 
     ! 3) v・T S u
-    ans_scalar = dot_product(v, matmul(T_mat, matmul(S_mat, u)))
-    write(output_unit, *) "1.2-3) v . T S u"
-    write(output_unit, '(G0)') ans_scalar
+    ! 指標表示: S_ij T_ki u_j v_k
+    ans_scalar = 0
+    do i = 1, 3
+        do j = 1, 3
+            do k = 1, 3
+                ans_scalar = ans_scalar + S_mat(i,j) * T_mat(k,i) * u(j) * v(k)
+            end do
+        end do
+    end do
+    call print_scalar("1.2-3) v . T S u", "S_ij T_ki u_j v_k", ans_scalar)
 
 contains
 
     ! ==========================================
-    ! サブルーチン・関数群 (すべて整数型で再定義)
+    ! サブルーチン・関数群
     ! ==========================================
 
-    ! 交代記号（エディントンのイプシロン） eps_ijk を返す関数
+    ! 交代記号 eps_ijk を返す関数
     integer function eps(i, j, k)
         integer, intent(in) :: i, j, k
-        if (i == j .or. j == k .or. k == i) then
-            eps = 0
-        else if ((i==1 .and. j==2 .and. k==3) .or. &
-                 (i==2 .and. j==3 .and. k==1) .or. &
-                 (i==3 .and. j==1 .and. k==2)) then
-            eps = 1
-        else
-            eps = -1
-        end if
+        eps = (i - j) * (j - k) * (k - i) / 2
     end function eps
 
-    ! ベクトル同士の外積: c = a x b
-    function cross_product(a, b) result(c)
-        integer, intent(in) :: a(3), b(3)
-        integer :: c(3)
-        c(1) = a(2)*b(3) - a(3)*b(2)
-        c(2) = a(3)*b(1) - a(1)*b(3)
-        c(3) = a(1)*b(2) - a(2)*b(1)
-    end function cross_product
+    ! スカラー出力
+    subroutine print_scalar(title, idx_str, s)
+        character(len=*), intent(in) :: title, idx_str
+        integer, intent(in) :: s
+        write(output_unit, '(A)') title
+        write(output_unit, '(A)') "  [Index notation]: " // idx_str
+        write(output_unit, '("    ", I6)') s
+    end subroutine print_scalar
 
-    ! ベクトル同士のテンソル積（直積）: C_ij = a_i * b_j
-    function dyadic_product(a, b) result(C)
-        integer, intent(in) :: a(3), b(3)
-        integer :: C(3,3)
-        integer :: i, j
-        do i = 1, 3
-            do j = 1, 3
-                C(i,j) = a(i) * b(j)
-            end do
-        end do
-    end function dyadic_product
-
-    ! テンソルとベクトルの外積: C_ij = A_ik * eps_jkl * b_l
-    function tensor_cross_vec(A, b) result(C)
-        integer, intent(in) :: A(3,3), b(3)
-        integer :: C(3,3)
-        integer :: i, j, k, l
-        C = 0
-        do i = 1, 3
-            do j = 1, 3
-                do k = 1, 3
-                    do l = 1, 3
-                        if (eps(j,k,l) /= 0) then
-                            C(i,j) = C(i,j) + A(i,k) * eps(j,k,l) * b(l)
-                        end if
-                    end do
-                end do
-            end do
-        end do
-    end function tensor_cross_vec
-
-    ! ベクトルをきれいに表示するサブルーチン
-    subroutine print_vec(title, v)
-        character(len=*), intent(in) :: title
+    ! ベクトル出力
+    subroutine print_vec(title, idx_str, v)
+        character(len=*), intent(in) :: title, idx_str
         integer, intent(in) :: v(3)
         write(output_unit, '(A)') title
-        write(output_unit, '(3(G0, 2X))') v
+        write(output_unit, '(A)') "  [Index notation]: " // idx_str
+        write(output_unit, '("    ", 3(I6, 2X))') v
     end subroutine print_vec
 
-    ! テンソル（行列）をきれいに表示するサブルーチン
-    subroutine print_mat(title, A)
-        character(len=*), intent(in) :: title
+    ! テンソル出力
+    subroutine print_mat(title, idx_str, A)
+        character(len=*), intent(in) :: title, idx_str
         integer, intent(in) :: A(3,3)
         integer :: r
         write(output_unit, '(A)') title
+        write(output_unit, '(A)') "  [Index notation]: " // idx_str
         do r = 1, 3
-            write(output_unit, '(3(G0, 2X))') A(r,:)
+            write(output_unit, '("    ", 3(I6, 2X))') A(r,:)
         end do
     end subroutine print_mat
 
